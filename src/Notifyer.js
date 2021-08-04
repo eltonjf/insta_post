e4cbf16698d0dd789d7c64f3ab3917daeea0eb1f
Elton Mário <eltonjf@gmail.com>
2021-08-04 00:00:00 -0300

    Emitter working

---
+++ src/Notifyer.js
@@ -0,0 +1,20 @@
+const Notifyer = {
+
+  async init() {
+      const permission = await Notification.requestPermission()
+      if(permission !== "granted") {
+        throw new Error('Permissão negada')
+      }
+  },
+  
+
+  notify({ title, body, icon }) {
+    return () => new Notification(title, {
+      body,
+      icon
+    })
+    
+  }
+
+}
+export { Notifyer }
